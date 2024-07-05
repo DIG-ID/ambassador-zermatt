d466dab4667540c0c11b7037b7226c6cffed9059
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-07-05 11:15:08 +0100

    up

--- search.php
+++ search.php
@@ -3,14 +3,14 @@ get_header();
 
 if ( have_posts() ) :
 	?>
-	<h1 class="page-title"><?php /* translators: %s: search query */ printf( esc_html__( 'Résultats de la recherche pour: %s', 'az' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
+	<h1 class="page-title"><?php /* translators: %s: search query */ printf( esc_html__( 'Résultats de la recherche pour: %s', 'ergopix' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
 	<?php
 	while ( have_posts() ) :
 		the_post();
 	endwhile;
 else :
 	?>
-	<p><?php esc_html_e( 'Désolé, mais rien ne correspond à vos critères de recherche. S\'il vous plaît essayez de nouveau avec des mots clés différents.', 'az' ); ?></p>
+	<p><?php esc_html_e( 'Désolé, mais rien ne correspond à vos critères de recherche. S\'il vous plaît essayez de nouveau avec des mots clés différents.', 'ergopix' ); ?></p>
 	<?php
 endif;
 
